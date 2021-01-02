6b4ccc121cfc6d21076f68c742178452f45f38bb
حصین <sh.danishyar@gmail.com>
2021-01-02 20:21:50 +0330

    ReOrdering modules and content

--- courses/urls.py
+++ courses/urls.py
@@ -6,9 +6,9 @@ app_name='courses'
 urlpatterns = [
     path('list/', views.ManageCourseListView.as_view(), name='manage_course_list'),
     path('create/', views.CourseCreateView.as_view(), name='course_create'),
-    path('<int:pk>/edit', views.CourseUpdateView.as_view(), name='course_edit'),
-    path('<int:pk>/delete', views.CourseDeleteView.as_view(), name='course_delete'),
-    path('<int:pk>/module', views.CourseModuleUpdateView.as_view(), name='course_module_update'),
+    path('<int:pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
+    path('<int:pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
+    path('<int:pk>/module/', views.CourseModuleUpdateView.as_view(), name='course_module_update'),
 
     path('module/<int:module_id>/content/<str:model_name>/create/',
         views.ContentCreateUpdateView.as_view(), name='module_content_create'),
@@ -16,5 +16,6 @@ urlpatterns = [
         views.ContentCreateUpdateView.as_view(), name='module_content_update'),
     path('content/<int:id>/delete/', views.ContentDeleteView.as_view(), name='module_content_delete'),
     path('module/<int:module_id>/', views.ModuleContentListView.as_view(), name='module_content_list'),
-
+    path('module/order/', views.ModuleOrderView.as_view(), name='module_order'),
+    path('content/order/', views.ContentOrderView.as_view(), name='content_order'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.views.generic.base import TemplateResponseMixin, View
 # to know more about django-braces you have check this out https://django-braces.readthedocs.io/
-from braces.views import LoginRequiredMixin, PermissionRequiredMixin
+from braces.views import LoginRequiredMixin, PermissionRequiredMixin, CsrfExemptMixin, JsonRequestResponseMixin
 from django.urls import reverse_lazy
 from django.forms.models import modelform_factory
 from django.apps import apps
@@ -191,3 +191,29 @@ class ModuleContentListView(TemplateResponseMixin, View):
                                     id=module_id,
                                     course__owner=request.user)
         return self.render_to_response({'module': module})
+
+
+# CsrfExemptMixin is to avoid checking for a csrf token in post request
+# we need this to perform ajax post request without generating csrf_token 
+# JsonRequestResponseMixin: passes request data as json and also serialize the response json 
+# and return HttpResponse with the application /json content type 
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(
+                                id=id,
+                                course__owner=request.user
+                                ).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(
+                id=id,
+                module__course__owner=request.user
+            ).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
